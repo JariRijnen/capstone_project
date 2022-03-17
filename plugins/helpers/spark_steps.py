10b8c62bdd78d19d9d838ab6ecbc2211b345e61f
Jari-Rijnen <jari-rijnen@digital_power.com>
2022-03-17 10:51:14 +0100

    refactoring

--- plugins/helpers/spark_steps.py
+++ plugins/helpers/spark_steps.py
@@ -1,41 +1,41 @@
 class SparkSteps:
     """Spark steps to execute on the EMR cluster."""
     SPARK_STEPS = [
-    {
-        "Name": "Move raw data from S3 to HDFS",
-        "ActionOnFailure": "CANCEL_AND_WAIT",
-        "HadoopJarStep": {
-            "Jar": "command-runner.jar",
-            "Args": [
-                "s3-dist-cp",
-                "--src=s3://{{ params.BUCKET_NAME }}/{{ params.wildfire_s3_data }}",
-                "--dest=/wildfires",
-            ],
+        {
+            "Name": "Move raw data from S3 to HDFS",
+            "ActionOnFailure": "CANCEL_AND_WAIT",
+            "HadoopJarStep": {
+                "Jar": "command-runner.jar",
+                "Args": [
+                    "s3-dist-cp",
+                    "--src=s3://{{ params.BUCKET_NAME }}/{{ params.wildfire_s3_data }}",
+                    "--dest=/wildfires",
+                ],
+            },
         },
-    },
-    {
-        "Name": "Pre-Process wildfire data",
-        "ActionOnFailure": "CANCEL_AND_WAIT",
-        "HadoopJarStep": {
-            "Jar": "command-runner.jar",
-            "Args": [
-                "spark-submit",
-                "--deploy-mode",
-                "client",
-                "s3://{{ params.BUCKET_NAME }}/{{ params.s3_script }}",
-            ],
+        {
+            "Name": "Pre-Process wildfire data",
+            "ActionOnFailure": "CANCEL_AND_WAIT",
+            "HadoopJarStep": {
+                "Jar": "command-runner.jar",
+                "Args": [
+                    "spark-submit",
+                    "--deploy-mode",
+                    "client",
+                    "s3://{{ params.BUCKET_NAME }}/{{ params.s3_script }}",
+                ],
+            },
         },
-    },
-    {
-        "Name": "Move clean data from HDFS to S3",
-        "ActionOnFailure": "CANCEL_AND_WAIT",
-        "HadoopJarStep": {
-            "Jar": "command-runner.jar",
-            "Args": [
-                "s3-dist-cp",
-                "--src=/output",
-                "--dest=s3://{{ params.BUCKET_NAME }}/{{ params.s3_clean }}",
-            ],
+        {
+            "Name": "Move clean data from HDFS to S3",
+            "ActionOnFailure": "CANCEL_AND_WAIT",
+            "HadoopJarStep": {
+                "Jar": "command-runner.jar",
+                "Args": [
+                    "s3-dist-cp",
+                    "--src=/output",
+                    "--dest=s3://{{ params.BUCKET_NAME }}/{{ params.s3_clean }}",
+                ],
+            },
         },
-    },
     ]
